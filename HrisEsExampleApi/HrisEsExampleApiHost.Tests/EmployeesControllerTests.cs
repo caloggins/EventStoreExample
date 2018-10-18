d60898476598c88ee190be123f09e235f87e14cf
Chris Loggins <cloggins@paylocity.com>
2018-10-17 22:19:27 -0500

    Framed in Employees
    Removed old values controller.
    Added scratch test for employees controller.

---
+++ HrisEsExampleApi/HrisEsExampleApiHost.Tests/EmployeesControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace HrisEsExampleApiHost.Tests
+{
+    public class EmployeesControllerTests
+    {
+        [Fact]
+        public void GetReturnsValue()
+        {
+            var sut = new EmployeesController();
+
+            var result = sut.Employees();
+
+            result.Value.Should().Be("Hello, world.");
+        }
+    }
+}
---
+++ HrisEsExampleApi/HrisEsExampleApiHost/EmployeesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HrisEsExampleApiHost
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+         [HttpGet]
+        public ActionResult<string> Employees()
+         {
+             return "Hello, world.";
+         }
+    }
+
+    public class Employee
+    {
+        public string Name => "Chris";
+    }
+}
